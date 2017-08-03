70718067fec418411b674c414f04cdb11ce5cdde
William Gardner <wg4568@gmail.com>
2017-08-02 22:10:29 -0700

    file API working

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var upload = multer({
 
 var app = express();
 
-// Do NOT push this code to GitHub
 const connection = mysql.createConnection({
     host: secret.host,
     user: secret.user,
@@ -24,24 +23,70 @@ connection.connect(function(error) {
     else console.log("Could not connect to MySQL database");
 });
 
+function createId(callback) {
+	var text = "";
+	var good = false;
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	function remake() {
+		text = "";
+
+		for (var i = 0; i < 32; i++) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+	}
+}
 
 app.post("/images", upload.single("image"), function(req, ret) {
-    var buffer = fs.readFileSync(req.file.path);
-    var size = buffer.length;
+	if (req.file) {
+		var string = fs.readFileSync(req.file.path).toString("hex").toUpperCase();
+		var title = req.body.title;
+		var uploader = req.body.uploader;
+		var tags = req.body.tags;
+		var type = req.file.mimetype;
 
-    console.log(size);
+		if (!title) title = "Untitled";
+		if (!uploader) uploader = "Anon";
+		if (!tags) tags = "";
 
-    // `name`, `uploader`, `uploaded`, `tags`, `id`, `image
-    connection.query("INSERT INTO images VALUES ?", {
-        name: "TEST IMAGE",
-        uploader: "WILLIAM",
-        uploaded: 21345,
-        tags: "5,sdsadsd,kkk",
-        image: size+","+buffer
-    }, function(error, result) {
-        if (error) ret.status(500).json(error);
-        ret.json(result);
-    });
+		connection.query(
+			"INSERT INTO `images`(`title`, `uploader`, `tags`, `id`, `type`, `image`) VALUES ('" + title + "', '" + uploader + "', '" + tags + "', UUID(), '" + type + "', x'" + string + "')",
+			function(error, result) {
+				if (error) ret.status(500).json(error);
+				ret.json(result);
+			}
+		);
+	} else {
+		ret.status(400).send("No image file");
+	}
+});
+
+app.get("/images/:id", function(req, ret) {
+	connection.query("SELECT `title`, `uploader`, `tags`, `type`, `id`, `upvotes`, `posted` FROM `images` WHERE ?", {id: req.params.id},
+	function(error, result) {
+		if (error) ret.status(500).json(error);
+		if (result.length > 0) {
+			ret.json(result[0]);
+		} else {
+			ret.status(404).send("No such image");
+		}
+	});
+});
+
+app.get("/images/:id/src", function(req, ret) {
+	connection.query("SELECT * FROM images WHERE ?", {id: req.params.id},
+	function(error, result) {
+		if (error) ret.status(500).json(error);
+		if (result.length > 0) {
+			ret.writeHead(200, {
+				"Content-Type": result[0].type,
+				"Content-Length": result[0].image.length
+			});
+			ret.end(new Buffer(result[0].image, 'binary'));
+		} else {
+			ret.status(404).send("No such image");
+		}
+	});
 });
 
 app.listen(8080, function() {
